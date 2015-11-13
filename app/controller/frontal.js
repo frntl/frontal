5bfd52a109067af2b5bb211be71da0f2c8228b08
sebastian-meier <contact@sebastianmeier.eu>
2015-11-13 23:41:00 +0100

    Removing my old ideas

---
+++ app/controller/frontal.js
@@ -0,0 +1,5 @@
+/*
+	there is already a package called app, so i called this module frontal
+	it will contain all the app initializing things
+*/
+"use strict";
---
+++ src/controller/frontal.js
@@ -0,0 +1,4 @@
+/*
+	there is already a package called app, so i called this module frontal
+	it will contain all the app initializing things
+*/
