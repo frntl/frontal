5f26a35f8e98d49ce3a8fc08a31bb3ee2f743239
fabiantheblind <icke@fabianmoronzirfas.me>
2015-11-13 15:34:56 +0100

    basic parsing is there. 	modified:   parser.js 	modified:   test.parser.js 	modified:   ../main.js 	modified:   ../../package.json 	modified:   ../../src/controller/parser.js 	modified:   ../../src/controller/test.parser.js

--- src/controller/test.parser.js
+++ src/controller/test.parser.js
@@ -1,5 +1,22 @@
-import * as parser from "./parser";
+/**
+ * This file is just for testing the functions
+ *
+ */
+import * as parser from './parser';
+import * as fs from 'fs';
+import * as md from 'markdown';
 
 // let res = parser.md("## test");
-let res = parser.json('{"one":"two"}');
-console.log(res);
+// let res = parser.json('{"one":"two"}');
+fs.readFile('./../../examples/presentation.md', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    let slides = parser.md2json(data);
+    for(let i = 0; i < slides.length;i++){
+      console.log(slides[i][0]);
+    }
+    // let slides = parser.md(data);
+    // console.log(`we found ${slides.length} slides` );
+  }
+});
