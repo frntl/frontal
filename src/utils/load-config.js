f07b1f997d9f364c29ad3232886997ae1e1a7993
fabiantheblind <icke@fabianmoronzirfas.me>
2016-08-08 14:44:41 +0200

    feat(): working on global frontmatter

--- src/utils/load-config.js
+++ src/utils/load-config.js
@@ -2,17 +2,20 @@ const yaml = require('js-yaml');
 import * as fs from 'fs';
 const fileExists = require('file-exists');
 
-
 function checkFile(p) {
   return fileExists(p) ? true : false;
 }
 // Get document, or throw exception on error
 export function yamlLoader(yamlFilePath) {
   let doc = null;
-  if(checkFile(yamlFilePath)) {
+  if (checkFile(yamlFilePath)) {
     let content = fs.readFileSync(yamlFilePath, 'utf8');
-    doc = yaml.safeLoad(content);
+    try {
+      doc = yaml.safeLoad(content);
+      console.log(doc);
+    } catch (err) {
+      throw err;
+    }
   }
-  console.log(doc);
   return doc;
 }
