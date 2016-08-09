f788fb70bee717dd283c2315a42b10e787a16b1e
fabiantheblind <icke@fabianmoronzirfas.me>
2016-08-09 13:32:00 +0200

    feat(styled intro): added styling to intro window
    #67

--- bin/build-help-html.js
+++ bin/build-help-html.js
@@ -4,6 +4,35 @@ const fs = require('fs');
 let pre = `<html>
 <head>
 <meta charset="UTF-8">
+<style>
+    body {
+  margin: 0 auto;
+  max-width: 50em;
+  font-family: mono;
+  line-height: 1.5;
+  padding: 4em 1em;
+  color: #555;
+  }
+  code,
+pre {
+   background: #f5f7f9;
+  border-bottom: 1px solid #d8dee9;
+  color: ##131618;
+
+
+}
+
+code {
+  padding: 2px 4px;
+  vertical-align: text-bottom;
+
+}
+
+pre {
+  padding: 1em;
+  border-left: 2px solid #69c;
+}
+</style>
 </head>
 <body>
 <pre style="word-wrap: break-word; white-space: pre-wrap;">`;
