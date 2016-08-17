6293195bf571d58e844b492298bcb4ec3eb640a7
fabiantheblind <icke@fabianmoronzirfas.me>
2016-08-17 11:08:06 +0200

    feat(logging): trying to integrate some logging features
    related to #77

---
+++ src/lib/logging.js
@@ -0,0 +1,10 @@
+// var log = require('electron-log');
+// const isDev = require('electron-is-dev');
+
+// export function logger (msg) {
+//   if(isDev) {
+//     console.log(msg);
+//   } else {
+//     log.info(msg);
+//   }
+// }
