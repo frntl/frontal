b3a83380cbabcf4eaec658fb333fea20b6c54629
fabiantheblind <icke@fabianmoronzirfas.me>
2016-08-29 15:50:38 +0200

    refactor(goto): added slide numbers on goto panel

--- src/views/renderer.js
+++ src/views/renderer.js
@@ -209,12 +209,14 @@ window.onload = () => {
   });
   ipcRenderer.on('goto', (event, arg) => {
     // decrease speakerNotes
-    smalltalk.prompt('Goto', 'Enter a slide number?', '1').then((value) => {
-      console.log(value);
-      currentSlide = !isNaN(parseInt(value, 10)) ? constrain(parseInt(value, 10) - 1, content.msg) : currentSlide;
-      setContent();
-    }, ()=> {
-      console.log('cancel');
-    });
+    smalltalk.prompt('Goto',
+      `Enter a slide number (${(currentSlide + 1)}/${content.msg.length}) ?`,
+      String(currentSlide + 1)).then((value) => {
+        console.log(value);
+        currentSlide = !isNaN(parseInt(value, 10)) ? constrain(parseInt(value, 10) - 1, content.msg) : currentSlide;
+        setContent();
+      }, ()=> {
+        console.log('cancel');
+      });
   });
 };
