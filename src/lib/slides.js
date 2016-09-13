f732ba44d4917234ba7cdcb608d5883683e031c7
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2016-09-13 16:02:38 +0200

    feat(speaker notes): images now also work in speaker notes
    close #96

--- src/lib/slides.js
+++ src/lib/slides.js
@@ -21,11 +21,16 @@ export function slides(data, config) {
 
   for (let i = 0; i < slds.length; i++) {
     let clean = removeHtmlComments((slds[i]));
-    clean.data = linker(clean.data, global.presetationRoot);
+    clean.data = linker(clean.data, global.presentationRoot);
     clean.data = hrefer(clean.data);
     let uncommented = [];
     for (let j = 0; j < clean.comments.length; j++) {
-      uncommented.push(marked(uncomment(clean.comments[j])));
+      let rmComments = uncomment(clean.comments[j]);
+      let html = marked(rmComments);
+      let linked = linker(html, global.presentationRoot);
+
+      let hrefed = hrefer(linked);
+      uncommented.push(hrefed);
     }
     let attr = null;
     if(useTomlConfig) {
